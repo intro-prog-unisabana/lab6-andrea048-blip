612e66ef727c060c303b77bb51405569f6cd1bb1
Andrea Santamaria <santamariaandrea048@gmail.com>
2026-03-31 14:25:38 -0500

    m

--- overall.py
+++ overall.py
@@ -1,10 +1,25 @@
+def student_averages(students):
+    result = {}
+
+    for student, grades in students.items():
+        if len(grades) == 0:
+            result[student] = 0
+        else:
+            total = sum(grades.values())
+            count = len(grades)
+            result[student] = round(total / count)
+
+    return result
+
+
 def assignment_averages(students):
     result = {}
 
-    # ✅ SOLUCIÓN CLAVE
+
     if len(students) == 0:
         return result
 
+    # Obtener tareas del primer estudiante
     first_student = next(iter(students.values()))
 
     for assignment in first_student.keys():
